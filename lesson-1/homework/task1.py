3b87f4fcf238cfb615142729879cb2fcbe6ad18a
ismoilovsamandar237-cmyk <ismoilovsamandar237@gmail.com>
2025-11-17 15:06:24 +0500

    Add task1.py with calculations for geometry and statistics

---
+++ lesson-1/homework/task1.py
@@ -0,0 +1,32 @@
+1
+side = 5
+perimeter = 4 * side
+area = side * side
+
+print("Perimeter:", perimeter)
+print("Area:", area)
+2
+import math
+
+diameter = 10
+circumference = math.pi * diameter
+
+print("Circumference:", circumference)
+3
+a = 6
+b = 10
+mean = (a + b) / 2
+
+print("Mean:", mean)
+4
+a = 4
+b = 3
+
+sum_ = a + b
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print("Sum:", sum_)
+print("Product:", product)
+print("Square of a:", square_a)
